f6c371847315e0f643195ea00cd3c6be535272d2
yangping.wyp <yangping.wyp@alibaba-inc.com>
2020-11-27 09:50:00 +0800

    Add test.

---
+++ spark2/src/test/java/org/apache/iceberg/iteblog/Compact.java
@@ -0,0 +1,61 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.iceberg.iteblog;
+
+import java.util.Map;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.iceberg.Table;
+import org.apache.iceberg.actions.Actions;
+import org.apache.iceberg.relocated.com.google.common.collect.Maps;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.internal.SQLConf;
+import org.apache.spark.sql.sources.v2.DataSourceOptions;
+
+import static org.apache.hadoop.hive.conf.HiveConf.ConfVars.METASTOREURIS;
+import static org.apache.iceberg.iteblog.TableTest.findTable;
+
+public class Compact {
+  private Compact() {
+  }
+
+  public static void main(String[] args) {
+    Configuration conf = new Configuration();
+    conf.set(METASTOREURIS.varname, "thrift://localhost:9083");
+
+    Map<String, String> maps = Maps.newHashMap();
+    maps.put("path", "default.iteblog");
+    DataSourceOptions options = new DataSourceOptions(maps);
+
+    Table table = findTable(options, conf);
+
+    SparkSession.builder()
+        .master("local[2]")
+        .config(SQLConf.PARTITION_OVERWRITE_MODE().key(), "dynamic")
+        .config("spark.hadoop." + METASTOREURIS.varname, "thrift://localhost:9083")
+        .config("spark.executor.heartbeatInterval", "100000")
+        .config("spark.network.timeoutInterval", "100000")
+        .enableHiveSupport()
+        .getOrCreate();
+
+    Actions.forTable(table).rewriteDataFiles()
+        .targetSizeInBytes(20 * 1024 * 1024) // 10KB
+        .execute();
+  }
+}
---
+++ spark2/src/test/java/org/apache/iceberg/iteblog/Person.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.iceberg.iteblog;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+public class Person implements Serializable {
+  private int id;
+  private String name;
+  private int age;
+  private Timestamp ts;
+
+  public Person() {
+  }
+
+  public Person(int id, String name, int age) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+  }
+
+  public Person(int id, String name, int age, Timestamp ts) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+    this.ts = ts;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public Timestamp getTs() {
+    return ts;
+  }
+
+  public void setTs(Timestamp ts) {
+    this.ts = ts;
+  }
+}
